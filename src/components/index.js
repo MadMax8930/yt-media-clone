3b4e6a4bc0f27c91b09a8e8f207734b4399fd844
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-06-06 23:52:26 +0200

    structure setup

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Navbar from "./Navbar";
+import Feed from "./Feed";
+import ChannelDetail from "./ChannelDetail";
+import VideoDetail from "./VideoDetail";
+import SearchFeed from "./SearchFeed";
+
+
+export { Navbar, Feed, SearchFeed, ChannelDetail, VideoDetail }
