d6425a8a7dc32d653a5492f7c114365786a36bc4
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-06-07 23:25:20 +0200

    routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Box } from '@mui/material';
-import { Navbar, MainPart, SearchFeed, YtVideoDetail, YtChannelDetail } from './components';
+import { Navbar, MainSection, SearchFeed, YtVideoDetail, YtChannelDetail } from './components';
 
 const App = () => (
     <BrowserRouter>
       <Box sx={{ backgroundColor: '#000'}}>
          <Navbar />
          <Routes>
-            <Route path='/' element={<MainPart />} />
+            <Route path='/' element={<MainSection />} />
             <Route path='/video/:id' element={<YtVideoDetail />} />
             <Route path='/channel/:id' element={<YtChannelDetail />} />
             <Route path='/search/:searchTerm' element={<SearchFeed />} />
