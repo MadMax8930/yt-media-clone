602ef4d87edd9840483aa2d577b839674fc495c3
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-06-08 02:16:00 +0200

    main bloc

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -4,8 +4,9 @@ import { Sidebar, Videos } from '../components';
 import { fetchFromAPI } from '../utils/fetchAPI';
 
 const MainSection = () => {
-   const [selectedCategory, setSelectedCategory] = useState('New');
+   const [selectedCategory, setSelectedCategory] = useState('Russkhof');
    const [videos, setVideos] = useState([])
+   
    useEffect(() => {
       fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
       .then(data => setVideos(data.items))
@@ -14,20 +15,20 @@ const MainSection = () => {
    return (
       <Stack sx={{ flexDirection: { sx: 'column', md: 'row'} }}>
          {/* Sidebar  */}
-         <Box sx={{ height: { sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }}>
-            <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
-            <Typography className='copyright' variant='body2' sx={{ mt: 1.5, color: '#fff'}}>
-               Copyright 2023 Russkhof Media
-            </Typography>
-         </Box>
+            <Box sx={{ height: { sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }}>
+               <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+               <Typography className='copyright' variant='body2' sx={{ mt: 1.5, color: '#fff'}}>
+                  Copyright 2023 Russkhof Media
+               </Typography>
+            </Box>
          {/* Videos */}
-         <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
-            <Typography variant='h4' fontWeight='bold' mb={2} style={{ color: 'white' }}>
-               {selectedCategory} {" "}
-               <span style={{ color: '#FC1503' }}>videos</span>
-            </Typography>
-            <Videos vids={videos} />
-         </Box>
+            <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+               <Typography variant='h4' fontWeight='bold' mb={2} style={{ color: 'white' }}>
+                  {selectedCategory} {" "}
+                  <span style={{ color: '#FC1503' }}>videos</span>
+               </Typography>
+               <Videos vids={videos} />
+            </Box>
       </Stack>
    )
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Paper, IconButton } from "@mui/material";
-import SearchIcon from "@mui/icons-material/Search"
+import { SearchIcon } from "../utils/constants";
 
 const SearchBar = () => (
    <Paper component="form" onSubmit={() => {}} sx={{ borderRadius: 20, border: '1px solid #e3e3e3', pl: 2, mr: { sm: 5 }, boxShadow: 'none'  }}>
