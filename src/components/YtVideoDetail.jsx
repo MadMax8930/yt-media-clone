a628a488d8695c9b364e5995f7a9be50672d73bc
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-06-09 01:22:15 +0200

    react player v1

--- src/components/YtVideoDetail.jsx
+++ src/components/YtVideoDetail.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import ReactPlayer from 'react-player';
+import { Box, Stack, Typography } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+import { Videos } from '../components';
+import { fetchFromAPI } from '../utils/fetchAPI';
+import { yt } from '../utils/constants';
 
 const YtVideoDetail = () => {
+   const { id } = useParams();
+   const [selectedVideo, setSelectedVideo] = useState(null);
+
+   useEffect(() => {
+      fetchFromAPI(`videos?part=snippet,statistics&id=${id}`)
+      .then(data => setSelectedVideo(data?.items[0]));
+   }, [id]);
+
   return (
-    <div>YtVideoDetail</div>
+    <Box minHeight="95vh">
+      <Stack direction={{ xs: 'column', md: 'row' }}>
+         <Box flex={1}>
+           <Box sx={{ width: '100%', position: 'sticky', top: '86px' }}>
+              <ReactPlayer url={yt + `/watch?v=${id}`} className="react-player" controls={true} />
+           </Box>
+         </Box>
+      </Stack>
+    </Box>
   )
 }
 
